969405b9bbb71133b3fca8bccdc1cf4f9e14f628
Tony So <tony.so.1116@gmail.com>
2018-05-12 08:41:00 -0700

    Smoothed lines of overall build success

--- source/kpimappers/Quality_Assurance/QaOverallBuildSuccessKpiMapper.ts
+++ source/kpimappers/Quality_Assurance/QaOverallBuildSuccessKpiMapper.ts
@@ -1,3 +1,5 @@
+import * as moment from "moment"
+import { avgPointsFunctions } from "./Functions/avgPointsFunctions"
 import { KpiMapper } from "../KpiMapper"
 import { IKpiState } from "../IKpiState"
 const config = require("../../../config/config")
@@ -5,13 +7,19 @@ const config = require("../../../config/config")
 /**
  * QaOverallBuildSuccessKpiMapper.
  * 
- * QA Overall Build Success vs Fail.
+ * QA Overall Build Success.
  */
 export class QaOverallBuildSuccessKpiMapper extends KpiMapper
 {
-    public readonly Title: string = "QA Overall Build Success vs Fail";
+    public readonly Title: string = "QA Overall Build Success";
 
     private _tablename: string = config.db.tablename.qa_builds_and_runs_from_bamboo;
+
+    private _from: string;
+    private _to: string;
+
+    // Start with splitting data points by how many days 
+    private dataPointsToPlot = 0;
 
     /**
      * Returns an array of SQL query strings given a date range.
@@ -23,6 +31,9 @@ export class QaOverallBuildSuccessKpiMapper extends KpiMapper
      */
     protected getQueryStrings(from: string, to: string, dateRange: number): string[]
     {
+        this._from = from;
+        this._to = to;
+
         return [`
         SELECT DATE_FORMAT(BUILD_COMPLETED_DATE, "%Y-%m-%d") AS Date, 
         AVG(CASE WHEN  BUILD_STATE = "Successful" THEN 1 ELSE 0 END) as Success
@@ -41,16 +52,70 @@ export class QaOverallBuildSuccessKpiMapper extends KpiMapper
     protected mapToKpiStateOrNull(jsonArrays: Array<any>[]): IKpiState|null
     {
         var jsonArray: Array<any> = jsonArrays[0];
+        var avgFunctions = new avgPointsFunctions();
+
+         // Invalid; One data point on a scatter chart shows nothing
+         if (jsonArray.length == 1)
+         {
+             return null;
+         }
+
+        // Declare how many points to plot
+        // Will always plot the first and last and in between will be
+        // a number that is equal to or lower than points specified
+        var plottingPoints = 30;
+
+        var overallPointsToAdd = 0;
+
         var successValue: Array<any> = [];
         var overallLabel: Array<any> = [];
+        var overallAverage: Array<any> = [];
 
         // Edit the stretch goal here
         const stretchGoal = 0.75;
 
-        for (let i: number = 0; i < jsonArray.length; ++i)
-        {
-            successValue.push(jsonArray[i].Success);
-            overallLabel.push(jsonArray[i].Date);
+         // 30 day difference example
+         var getDaysLeft = avgFunctions.getHowManyDaysLeft(this._from, this._to);
+
+         // Find out how many data points to split across using Math.Floor
+        this.dataPointsToPlot = Math.floor((getDaysLeft/plottingPoints));
+
+        var overallFirstPush = false;
+
+        for (let i: number = 0; i < jsonArray.length; ++i) {
+            // get the starting point of the graph and add it in
+
+            if (!overallFirstPush) {
+                successValue.push(jsonArray[i].Success);
+                overallLabel.push(jsonArray[i].Date);
+                overallFirstPush = true;
+            } else {
+                // If there is more points than how many days left to plot
+                if (getDaysLeft < plottingPoints) {
+                    // plot it normally
+                    successValue.push(jsonArray[i].Success);
+                    overallLabel.push(jsonArray[i].Date);
+                } else {
+                    // Implement crazy logic here
+                    overallAverage.push(jsonArray[i].Success);
+                    ++overallPointsToAdd;
+                    // Once it meets the specific points then it'll add to the value and label
+                    // or if it is the final point
+                    if ((overallPointsToAdd % this.dataPointsToPlot == 0) || (moment(this._to).diff(jsonArray[i].Date,'days') == 0)) {
+                        var averageValue = avgFunctions.getAveragePercentage(overallAverage);
+                        successValue.push(averageValue);
+                        overallLabel.push(jsonArray[i].Date);
+
+                        // clean everything up once it's over
+                        // resets the points to 0
+                        overallPointsToAdd = 0;
+
+                        // clean data conained by popping from the array
+                        avgFunctions.cleanAverageData(overallAverage); 
+                    } // end if statement
+                } // end else statement
+            } // end crazy if statement
+            
         }
 
         return {
